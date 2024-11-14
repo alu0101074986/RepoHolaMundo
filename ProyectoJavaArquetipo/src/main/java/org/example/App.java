1da23f9761b6f682259ae7d4970de60a090478d6
Marcelo Daniel Choque Mamani <72565315+alu0101074986@users.noreply.github.com>
2024-11-14 10:57:32 +0000

    Update App.java

--- ProyectoJavaArquetipo/src/main/java/org/example/App.java
+++ ProyectoJavaArquetipo/src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.util.Random;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World con Arquetipo!" );
+        // Lista de mensajes
+        String[] mensajes = {
+            "Hola Mundo con Arquetipo!",
+            "¡Bienvenido al mundo de Java!",
+            "¡Programación en Java es divertida!",
+            "¡Hola desde tu aplicación de ejemplo!",
+            "¡Este es un mensaje aleatorio!"
+        };
+
+        // Generador de números aleatorios
+        Random random = new Random();
+        int indice = random.nextInt(mensajes.length);
+
+        // Imprime un mensaje aleatorio
+        System.out.println(mensajes[indice]);
     }
 }
